bc231b7018c79f63300048f20124c5e251c27e71
McBainCO <grahammcbain@gmail.com>
2014-09-03 09:55:14 -0600

    errors fixed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,11 +8,16 @@ class RegistrationsController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if
+    @user.errors[:email] << "Missing"
+      @user.errors[:password] << "Missing"
+      @user.errors[:name] << "Missing"
+      @user.errors[:password_confirmation] << "Missing"
+      render :new
+    else
+      @user.save
       session[:user_id] = @user.id
       redirect_to root_path
-    else
-      render :new
     end
   end
 
